d1d3108ec11bce28aad7b3a1ed099073313dc1a3
Abhishek <pundirabhishek987@gmail.com>
2023-01-13 16:14:43 +0530

    Add heading

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,39 +1,12 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number | string ;
-let isStudent: boolean ;
-let hobbies: string[];
-let role:[number, string];// tuple
-name = "Abhishek";
-age=21;
-age="anup";
 
-isStudent=true;
-hobbies=["Coding", "Swimming"];
-role=[5,"Abhishek"];
 
-// declaring obects: we have 2 ways to declare objects
-// method 1
-let person1: Object;
-
-// method 2
-
-type Person = {
-  name: String;
-  age?: number;
-}
-
-let person2: Person={
-  name: "Abhishek",
-}
-
-function App() {
+const App : React.FC= () => {
   return (
     <div className="App">
-      <h1>Hello {name}, Welcome! </h1>
-      <h2>Age: {age},  Student: {isStudent}, hobbies: {hobbies},  role: {role}</h2>
+      <span className='heading'>Taskify</span>
     </div>
   );
 }
